51c97c668e5320d7a39befa6e637e8e38ce31f20
ArunNejakar <arunnejakar847@gmail.com>
2025-11-24 16:25:01 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -4,12 +4,11 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) == 3:  
         x = int(sys.argv[1])
         y = int(sys.argv[2])
-    else:
-        x =10
+    else:  
+        x = 10
         y = 20
-        print("sum", add(x, y))
-        
-        
+
+    print("sum", add(x, y))
